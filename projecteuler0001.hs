128a33f4397e4cf661d91d2bc508e3df0cd3c836
cijb-7724 <johokamia0@gmail.com>
2023-11-08 02:16:50 +0900

    q1 by haskell

--- projecteuler0001.hs
+++ projecteuler0001.hs
@@ -1,13 +1,9 @@
-main =
-	print (calc a)
-
+main = print (calc a)
 
 a = [1..1000-1]
-
 calc :: [Int] -> Int
 calc [] = 0
 calc (hd:tl)
 	| mod hd 3 == 0 = hd + calc tl
 	| mod hd 5 == 0 = hd + calc tl
 	| otherwise = calc tl
-
