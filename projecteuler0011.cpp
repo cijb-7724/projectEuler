9d35d63f4233c235fe65132ce8265cd854878542
cijb-7724 <johokamia0@gmail.com>
2023-11-08 11:26:47 +0900

    q11

---
+++ projecteuler0011.cpp
@@ -0,0 +1,380 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <math.h>
+#include <string>
+#include <cstdlib>
+#include <iomanip> //出力桁数
+#include <queue> // queue
+#include <stack> // stack
+#include <deque> // deque
+#include <sstream> //基数変換
+#include <bitset> //2進数に変換
+
+
+using namespace std;
+
+#define pi 3.14159265358979323846
+#define yes "Yes"
+#define no "No"
+#define yesno(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define alphabet "abcdefghijklmnopqrstuvwxyz"
+#define Alpahbet "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repp(i, l, r) for (int i = l; i < r; ++i)
+#define rrep(i, r, l) for (int i = r; i >= l; --i)
+#define tp() cout << "here~~" << endl
+#define el '\n'
+
+const int dx[4] = {1, 0, -1, 0};//グリッド上の探索
+const int dy[4] = {0, 1, 0, -1};//上下左右移動方向
+const int dxdi[4] = {1, 1, -1, -1};//グリッド上の探索
+const int dydi[4] = {1, -1, 1, -1};//斜め移動方向
+
+
+//型エイリアス vector<set<pair<tuple : bool<char<string<int<ll<ull
+using ll = long long;
+using ull = unsigned long long;
+using vb = vector<bool>;
+using vc = vector<char>;
+using vs = vector<string>;
+using vi = vector<int>;
+using vll = vector<long long>;
+using si = set<int>;
+using sll = set<ll>;
+using msi = multiset<int>;
+using msll = multiset<ll>;
+using mss = multiset<string>;
+using pii = pair<int, int>;
+using pill = pair<int, ll>;
+using plli = pair<ll, int>;
+using pllll = pair<long long, long long>;
+using vvb = vector<vector<bool>>;
+using vvc = vector<vector<char>>;
+using vvs = vector<vector<string>>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+using vsi = vector<set<int>>;
+using vpii = vector<pair<int, int>>;
+using vpllll = vector<pair<long long, long long>>;
+using spii = set<pair<int, int>>;
+
+using stst = stringstream;
+
+// [ノード][(接続ノード, 重み)]
+using Graph = vector<vector<int>>;//重みなしグラフ構造
+using GraphWeight = vector<vector<pair<int,int>>>;//重みありグラフ構造
+using GraphCh = vector<vector<char>>;//charGridの探索
+using GraphIn = vector<vector<int>>;//charGridの探索
+using GridPos = pair<int, int>;//グリッド上の位置・座標
+
+//関数定義群
+vector<pair<long long, long long> > prime_factorize(long long); //素因数分解
+bool contain_string(string, string); //部分文字列の一致判定
+ll gcd(ll, ll); //最大公約数
+ll lcm(ll, ll); //最小公倍数
+ll powll(ll, ll); //llの累乗
+bool in_table(int, int); //添え字が１次元配列内か判定
+bool in_table(int, int, int, int); //添え字が２次元配列内か判定
+ll minll(ll, ll);
+ll maxll(ll, ll);
+ll modPow(ll, ll, ll);
+ll modInv(ll, ll);
+
+
+
+//Union Find
+struct UnionFind {//頂点数N, クエリ数Q -> O(Q log N)
+    vi p; //自分の親を管理
+    vi r;
+    UnionFind(int n) { //コンストラクタ インスタンス生成時に初期化
+        p.resize(n);
+        rep(i, n) {
+            p[i] = i;
+        }
+        r.resize(n, 1);
+    }
+    int find(int x) { // 均し計算量O(log N)
+        if (p[x] == x) return x;
+        else return p[x] = find(p[x]);//パス圧縮　自分の親を代表元に張りなおす
+    }
+    void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return;
+        if (r[x] > r[y]) {// Union by rank
+            swap(x, y);
+        }
+        if (r[x] == r[y]) {
+            ++r[y];
+        }
+        p[x] = y;
+    }
+};
+
+
+
+
+// vb seen;
+// void dfs (const Graph &G, int v) {
+//     seen[v] = true;
+//     //cout << "v = " << v+1 << el;
+//     for (auto nextv : G[v]) {
+//         if (seen[nextv] == true) {
+//             continue;
+//         }
+//         dfs(G, nextv);
+//     }
+// }
+
+// vi dist;
+// int bfs (const Graph &G, int v) {
+//     queue<int> que;
+//     dist[v] = 0;
+//     que.push(v);
+//     while(!que.empty()) {
+//         v = que.front();
+//         que.pop();
+//         // cout << "v = " << v+1 << el;
+//         for(int nv: G[v]) {
+//             if (dist[nv] == -1) {
+//                 dist[nv] = dist[v] + 1;
+//                 que.push(nv);
+//             } else {
+//                 //tuiki
+//                 continue;
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
+/*
+-----
+---------------
+-------------------------
+---------------
+-----
+*/
+
+
+
+/*
+problem 11
+in the 20*20 grid below, four numbers along a diagonal line
+have been marked in re
+
+the product of these numbers is 26*63*78*14 = 1788696.
+what is the greatest product of four adjacent numbers
+in the same direction(up, down, left, right or diagonally)
+in the 20*20 grid?
+*/
+void putOutVVI () {
+    string s = 
+    "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08\
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00\
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65\
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91\
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80\
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50\
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70\
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21\
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72\
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95\
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92\
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57\
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58\
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40\
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66\
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69\
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36\
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16\
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54\
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48";
+    cout << s << endl;
+    int n = 20;
+    int m = n*3-1;
+    for (int i=0; i<n; ++i) {
+        cout << "{";
+        for (int j=0; j<m; ++j) {
+            if (j%3==2) cout << ",";
+            if (j%3==0 && s[i*m+j] == '0') continue;
+            cout << s[i*m+j];
+        }
+        cout << "},\n";
+    }
+}
+int main() {
+    int n = 20;
+    vvi table = {
+        {8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8},
+        {49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0},
+        {81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65},
+        {52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91},
+        {22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80},
+        {24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50},
+        {32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70},
+        {67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21},
+        {24, 55, 58, 5, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72},
+        {21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95},
+        {78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92},
+        {16, 39, 5, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57},
+        {86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58},
+        {19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40},
+        {4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66},
+        {88, 36, 68, 87, 57, 62, 20, 72, 3, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69},
+        {4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36},
+        {20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16},
+        {20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54},
+        {1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48}
+    };
+    // putOutVVI();
+    int ans = 0;
+    for (int i=-1; i<=1; ++i) {
+        for (int j=-1; j<=1; ++j) {
+            if (i == 0 && j == 0) continue;
+            for (int starti=0; starti<n; ++starti) {
+                for (int startj=0; startj<n; ++startj) {
+                    int k = 1;
+                    for (int len=0; len<4; ++len) {
+                        if (!in_table(starti+i*len, startj+j*len, n, n)) break;
+                        k *= table[starti+i*len][startj+j*len];
+                    }
+                    ans = max(ans, k);
+                }
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+/*
+the answer is 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//関数群---------------------------------------------------
+
+ 
+// 素因数分解
+// 460 = 2^2 x 5 x 23 の場合
+// 返り値は {{2, 2}, {5, 1}, {23, 1}}
+vector<pair<long long, long long> > prime_factorize(long long N) {
+    // 答えを表す可変長配列
+    vector<pair<long long, long long> > res;
+
+    // √N まで試し割っていく
+    for (long long p = 2; p * p <= N; ++p) {
+        // N が p で割り切れないならばスキップ
+        if (N % p != 0) {
+            continue;
+        }
+
+        // N の素因数 p に対する指数を求める
+        int e = 0;
+        while (N % p == 0) {
+            // 指数を 1 増やす
+            ++e;
+
+            // N を p で割る
+            N /= p;
+        }
+
+        // 答えに追加
+        res.emplace_back(p, e);
+    }
+
+    // 素数が最後に残ることがありうる
+    if (N != 1) {
+        res.emplace_back(N, 1);
+    }
+    return res;
+}
+
+//文字列sにtが含まれているかを判定する
+// s.size() >= t.size()に注意
+bool contain_string(string s, string t) {
+    if ((int)s.size() < (int)t.size()) {
+        return false;
+    }
+    int cnt;
+    for (int i=0;i<(int)s.size(); i++) {
+        cnt = 0;
+        for (int j = 0; j<(int)t.size(); j++) {
+            if (i+j <(int)s.size() && s[i+j] == t[j]) {
+                cnt ++;
+            }
+        }
+        if (cnt == (int)t.size()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+ll gcd(ll a, ll b) {
+    if (a < b) {
+        ll tmp = a;
+        a = b;
+        b = tmp;
+    }
+    while(b) {
+        ll aa = b;
+        ll bb = a%b;
+        a = aa;
+        b = bb;
+    }
+    return a;
+}
+ll lcm(ll a, ll b) {
+    return a*b/gcd(a, b);
+}
+ll powll(ll a, ll b) {
+    ll k = 1;
+    rep(i, b) {
+        k *= a;
+    }
+    return k;
+}
+ll modPow(ll a, ll n, ll mod) {
+    ll res = 1;
+    while(n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+ll modInv(ll a, ll mod) {
+    return modPow(a, mod-2, mod);
+}
+bool in_table(int i, int h) {
+    if (i<0 || i>=h) {
+        return false;
+    } else {
+        return true;
+    }
+}
+bool in_table(int i, int j, int h, int w) {
+    if (i<0 || i>=h || j<0 || j>=w) {
+        return false;
+    }
+    return true;
+}
+
