cd6389e6848834bda1e4d9a4f0c6f3f32762a8d9
cijb-7724 <johokamia0@gmail.com>
2023-11-12 03:03:04 +0900

    yet

--- projecteuler0004.hs
+++ projecteuler0004.hs
@@ -5,4 +5,16 @@ b = [100..999]
 c = [x*y|x<-a, y<-b]
 
 judge num
-	tostring
+	if n > 100000
+	then
+		na = div n 100000
+		nb = mod (div n 10000) 10
+		nc = mod (div n 1000) 10
+		nd = mod (div n 100) 10
+		ne = mod (div n 10) 10
+		nf = mod n 10
+		if (na == nf && nb == ne && nc == nd)
+		then = True
+		else = False
+	else
+		= False
